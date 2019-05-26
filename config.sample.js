54f769103e43945562c2a49169bad006ea17df57
Yves Hoppe <yves.hoppe@community.joomla.org>
2019-05-26 12:09:24 +0200

    [Testing] Added travis

--- config.sample.js
+++ config.sample.js
@@ -2,7 +2,7 @@
  * Rename to config.js and adjust
  */
 const config = {
-  mongodb_host: '127.0.0.1',
+  mongodb_host: 'localhost',
   mongodb_user: '',
   mongodb_password: '',
   mongodb_auth_mechanism: 'DEFAULT',
