d9397455a76678fed348f6ba429ce7b23570ff31
Yves Hoppe <yves.hoppe@community.joomla.org>
2019-05-27 11:28:40 +0200

    Clean up tags and categories route

--- routes/categories.js
+++ routes/categories.js
@@ -5,13 +5,6 @@ const QuestionModel = require('../models/question');
 
 /* GET distinct categories. */
 router.get('/', async (req, res, next) => {
-  let filter = {};
-
-  // Apply published filter for requests
-  if (!req.authenticated) {
-    filter.published = true;
-  }
-
   const categories = await QuestionModel.distinct('category').exec();
 
   res.json(categories);
--- routes/tags.js
+++ routes/tags.js
@@ -3,15 +3,8 @@ const router = express.Router();
 
 const QuestionModel = require('../models/question');
 
-/* GET tags categories. */
+/* GET distinct tags */
 router.get('/', async (req, res, next) => {
-  let filter = {};
-
-  // Apply published filter for requests
-  if (!req.authenticated) {
-    filter.published = true;
-  }
-
   const tags = await QuestionModel.distinct('tags').exec();
 
   res.json(tags);
